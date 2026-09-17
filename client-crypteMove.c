1ec7cb5de4efec1ab3bc3173e3248553081f3389
jallouliahmed998-hash <jallouliahmed998@gmail.com>
2026-09-18 00:02:02 +0200

    Add files via upload

--- client-crypteMove.c
+++ client-crypteMove.c
@@ -1,9 +1,6 @@
 #include "client.h"
 #include <stdio.h>
-#include <ctype.h>
-#include <stdbool.h>
 #include <string.h>
-#include <stdlib.h>
 
 void remove_first_char(char *str) {
     if (str == NULL || str[0] == '\0') {
@@ -14,58 +11,58 @@ void remove_first_char(char *str) {
 }
 
 void move_n_first_char_to_end(char *str, int n) {
-    size_t len;
     if (str == NULL || str[0] == '\0' || n <= 0) {
         return;
     }
-    len = strlen(str);
-    n=n%len;
-    char new[n+1];
-    memmove(new, str, n);
-    for (int i=0;i<n;i++){
-    remove_first_char(str);}
-    strcat(str,new);
+    size_t len = strlen(str);
+    
+    // On sort s'il ne reste pas au moins n caractères
+    if (len < (size_t)n) {
+        return;
+    }
+
+    char new[n + 1];
+    memcpy(new, str, n);
+    new[n] = '\0'; //  car memcpy n'ajoute pas de '\0'
+    memmove(str, str + n, len - n + 1);
+    strcat(str, new);
 }
 
-
-
-int main(){
+int main(void) {
     // Affiche les échanges avec le serveur (false pour désactiver)
     show_messages(true);
 
     // Connexion au serveur AppoLab
     connexion("im2ag-appolab.u-ga.fr");
+
     char reponse[MAXREP];
-    // Remplacez <identifiant> et <mot de passe> ci dessous.
-    envoyer("login 12518371 RAHMANI");
+    char enc[MAXREP] = ""; 
+
+    envoyer("login 12513439 JALLOULI");
     envoyer("load crypteMove");
-    envoyer_recevoir("help",reponse);
-    char enc[MAXREP] = "";
-    while (strlen(reponse)>0){
-        
-        //step 1 ///////
-        
+    envoyer_recevoir("help", reponse);
 
-        char c= reponse[0];
-        char temp[2];
-        temp[0]=c;
-        temp[1]='\0';
-        strcat(enc,temp);
+    int idx_enc = 0;
+    while (reponse[0] != '\0') {
+        // STEP 1
+        char c = reponse[0];
+        enc[idx_enc++] = c;
+        enc[idx_enc] = '\0';
 
-        //step 2 //////////////////////
+        // STEP 2
+        remove_first_char(reponse);        
+        int x = (unsigned char)c % 8;
+        move_n_first_char_to_end(reponse, x);
+    }
 
+    
+    envoyer("depart");
 
-        remove_first_char(reponse);
-        printf("Message après suppression du premier caractère : %s\n",reponse);
-        int x=c%8;
-        move_n_first_char_to_end(reponse,x);
-        printf("Message après transformation : %s\n",reponse);
-        }
+    
     envoyer(enc);
 
-    printf ("Fin d'envoi des messages.\n");
-    printf ("Pour envoyer d'autres lignes, ajouter des appels à la fonction `envoyer`\n");
+    printf("Fin d'envoi des messages.\n");
     deconnexion();
-    printf ("Fin de la connection au serveur\n");
+    printf("Fin de la connection au serveur\n");
     return 0;
 }

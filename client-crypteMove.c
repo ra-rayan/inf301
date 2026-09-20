11260997b5a87e3f0237e7fdb91e6c8da60a99d4
jallouliahmed998-hash <jallouliahmed998@gmail.com>
2026-09-20 20:08:10 +0200

    Add files via upload

--- client-crypteMove.c
+++ client-crypteMove.c
@@ -1,78 +1,59 @@
 #include "client.h"
 #include <stdio.h>
-#include <ctype.h>
-#include <stdbool.h>
 #include <string.h>
-#include <stdlib.h>
 
 void remove_first_char(char *str) {
     if (str == NULL || str[0] == '\0') {
         return;
     }
-
     memmove(str, str + 1, strlen(str));
 }
 
-void move_n_first_char_to_end(char *str, int n){
-    if (str == NULL)
+void move_n_first_char_to_end(char *str, int n) {
+    if (str == NULL || str[0] == '\0' || n <= 0) {
         return;
-    int len = strlen(str);
-    if (len == 0)
+    }
+    size_t len = strlen(str);
+    if (len < (size_t)n) {
         return;
-    if (n >= len)
-    return;
-    char temp[len + 1];
-    int i = n;
-    while (i < len){
-        temp[i - n] = str[i];
-        i++;
     }
-    while (i < len + n){
-        temp[i - n] = str[i - len];
-        i++;
-    }
-    temp[len] = '\0';
-    strcpy(str, temp);
+
+    char new[n + 1];
+    memcpy(new, str, n);
+    new[n] = '\0';
+    memmove(str, str + n, len - n + 1);
+    strcat(str, new);
 }
 
-
-
-int main(){
-    // Affiche les échanges avec le serveur (false pour désactiver)
+int main(void) {
     show_messages(true);
 
-    // Connexion au serveur AppoLab
     connexion("im2ag-appolab.u-ga.fr");
+
     char reponse[MAXREP];
-    // Remplacez <identifiant> et <mot de passe> ci dessous.
-    envoyer("login 12518371 RAHMANI");
-    envoyer("load crypteMove");
-    envoyer_recevoir("help",reponse);
-    envoyer("depart");
     char enc[MAXREP] = "";
-    while (strlen(reponse)>0){
-        
-        //step 1 ///////
-        
 
-        char c= reponse[0];
-        char temp[2];
-        temp[0]=c;
-        temp[1]='\0';
-        strcat(enc,temp);
+    envoyer("login 12513439 JALLOULI");
+    envoyer("load crypteMove");
 
-        //step 2 //////////////////////
+    envoyer_recevoir("aide", reponse);
 
+    int idx_enc = 0;
+    while (strlen(reponse) > 0) {
+        char c = reponse[0];
+        enc[idx_enc++] = c;
+        enc[idx_enc] = '\0';
 
         remove_first_char(reponse);
-        int x=c%8;
-        move_n_first_char_to_end(reponse,x);
-        }
+
+        int x = (unsigned char)c % 8;
+        move_n_first_char_to_end(reponse, x);
+    }
+
+    
+    envoyer("depart");
     envoyer(enc);
 
-    printf ("Fin d'envoi des messages.\n");
-    printf ("Pour envoyer d'autres lignes, ajouter des appels à la fonction `envoyer`\n");
     deconnexion();
-    printf ("Fin de la connection au serveur\n");
     return 0;
 }
